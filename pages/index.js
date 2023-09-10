e84df019dfbd1ad4c509f0af5a13c61e7f47529c
Dhananjay Surti <dhananjaysurti@Dhananjays-MacBook-Pro.local>
2023-09-10 19:43:38 -0400

    Set up project structure

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+};
+
+export default HomePage;
