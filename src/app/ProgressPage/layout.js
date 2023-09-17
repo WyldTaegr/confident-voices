f567ee072beb870e8e55c8c433e2aca20a46c6d5
rachelsyamamoto <rachelsyamamoto@gmail.com>
2023-09-17 01:13:25 -0400

    Progress Page template with 3 graphs

--- src/app/ProgressPage/layout.js
+++ src/app/ProgressPage/layout.js
@@ -1,19 +1,12 @@
 import React from 'react';
-//import Button from 'react-bootstrap/Button'
-//import color and css styles
-import "./color.css";
 
 export default function ProgressPageLayout({children}){
 	return (
 		
-		<div>
-			{children}
-			<button>
-				<p>
-				This button literally does nothing
-				</p>
-			</button>
-		</div>
+			<div>
+				{children}
+			</div>
+		
 	)
 
 };
