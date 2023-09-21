19633e5b85597d79cf33fe1bbc848c1a0ba79fc4
Tiger Tian <tst25@case.edu>
2023-09-21 15:09:54 -0700

    Remove SSR from aws configure

--- src/app/LoginPage/page.js
+++ src/app/LoginPage/page.js
@@ -5,7 +5,7 @@ import { useAuthenticator, useTheme } from '@aws-amplify/ui-react';
 import { View, Image, Text, Heading, Button, SelectField, CheckboxField } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from '@/aws-exports';
-Amplify.configure({ ...awsExports, ssr: true });
+Amplify.configure(awsExports);
 
 export default function LoginPage() {
   return (
