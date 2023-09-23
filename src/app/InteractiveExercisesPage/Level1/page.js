468f90d09d994008a2c406c661852d39c4ab57fe
Dhananjay Surti <dhananjaysurti@Dhananjays-MacBook-Pro.local>
2023-09-23 15:40:51 -0400

    Set up basic logic for how questions will look

--- src/app/InteractiveExercisesPage/Level1/page.js
+++ src/app/InteractiveExercisesPage/Level1/page.js
@@ -5,7 +5,21 @@ import Link from 'next/link';
 const Level1Page = () => {
   return (
     <div>
-      <h1> Level 1</h1>    
+      <h1> Level 1</h1>
+      {
+        /**
+         * Have a for loop that iterates through the questions which are stored in database
+         * Would perhaps have a form of some sort that stores the recordings in S3 bucket.
+         * The recordings would be mapped to the question which was answered.
+         * Format would have to be something like:
+         * Question......
+         * voice recording
+         * Question.......
+         * voice recording
+         * Question.......
+         * voice recording
+         */
+      }
     </div>
   );
 };
