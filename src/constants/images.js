b482333807409c9a5de01f86c35f76b42cc8b9db
das-isha <ixd82@case.edu>
2023-10-01 21:16:22 -0400

    profiles of us

--- src/constants/images.js
+++ src/constants/images.js
@@ -21,8 +21,10 @@ const leadingHealthProvider = require("/Users/ishadas/Desktop/confident-voices/s
 const downloadApp = require("/Users/ishadas/Desktop/confident-voices/src/app/publcifold/impassets/images/down-app.svg");
 
 // testimonial
-const testinomialAvatar = require("/Users/ishadas/Desktop/confident-voices/src/app/publcifold/impassets/images/testimonial-avatar.png");
-
+const testimonialAvatar = require("/Users/ishadas/Desktop/confident-voices/src/app/publcifold/impassets/images/testimonial-avatar.png");
+const testimonialAvatar2 = require("/Users/ishadas/Desktop/confident-voices/src/app/publcifold/impassets/images/testimonial-avatar2.png");
+const testimonialAvatar3 = require("/Users/ishadas/Desktop/confident-voices/src/app/publcifold/impassets/images/testimonial-avatar3.png");
+const testimonialAvatar4 = require("/Users/ishadas/Desktop/confident-voices/src/app/publcifold/impassets/images/testimonial-avatar4.png");
 // articles
 const article1 = require("/Users/ishadas/Desktop/confident-voices/src/app/publcifold/impassets/images/article-1.png");
 const article2 = require("/Users/ishadas/Desktop/confident-voices/src/app/publcifold/impassets/images/article-2.png");
@@ -44,5 +46,8 @@ export default {
 	article1,
 	article2,
 	article3,
-	testinomialAvatar,
+	testimonialAvatar,
+	testimonialAvatar2,
+	testimonialAvatar3,
+	testimonialAvatar4,
 };
