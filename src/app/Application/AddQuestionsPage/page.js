aee333c2c4cb25c8ff8a9cd200d0388a70a8e19f
Dhananjay Surti <djsurti3003@gmail.com>
2023-10-07 09:53:33 -0400

    Create a Page to create text

--- src/app/Application/AddQuestionsPage/page.js
+++ src/app/Application/AddQuestionsPage/page.js
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { Amplify } from 'aws-amplify';
 import { API, graphqlOperation } from 'aws-amplify';
@@ -8,18 +8,31 @@ import { Button } from '@aws-amplify/ui-react';
 import awsExports from '@/aws-exports';
 Amplify.configure(awsExports);
 
-
-async function questionCreation(){
-  const newQuestion = await API.graphql(
-    graphqlOperation(mutations.createQuestion, { input: {} })
-  );
-}
-
 const AddQuestionsPage = () => {
+
+  async function questionCreation(){
+    const newQuestion = await API.graphql(
+      graphqlOperation(mutations.createQuestion, { input: "hello" })
+    );
+  }
+
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <div>
-      <h1> Add questions page</h1>    
-      <input></input>
+      <form onSubmit={questionCreation}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter something"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
