91c46eb4e81cf091b3c898fd4371490d54292564
das-isha <ixd82@case.edu>
2023-10-08 12:26:32 -0400

    landing page changes

--- src/app/components/FooterLinkSet.js
+++ src/app/components/FooterLinkSet.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import * as styles from "../../styles/Footer.module.css";
+//made it 0 this was showing errors
 const FooterLinkSet = ({ linkSet }) => {
 	return (
 		<div className={`${styles.linkGroup}`}>
 			<h4> </h4>
 			<div>
 				<ul className={`${styles.footerLinks}`}>
-					//made it 0 this was showing errors
 					{Array(0)
 						.fill("Click to go to top of Page")
 						.map((item, index) => {
