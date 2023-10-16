1f3fbe5c9f6ec499668a0b3e053eee37fdc38586
Tiger Tian <tst25@case.edu>
2023-10-15 17:09:02 -0700

    Add login check to application layout

--- src/app/Application/layout.js
+++ src/app/Application/layout.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import { Amplify, Auth } from 'aws-amplify';
+import { redirect } from 'next/navigation';
+import awsExports from '@/aws-exports';
+Amplify.configure(awsExports);
 
-export default function ApplicationLayout({children}){
+export default async function ApplicationLayout({children}){
+	
+	try {
+		console.log(await Auth.currentAuthenticatedUser());
+	} catch (e) {
+		if (e != "The user is not authenticated") {
+			console.log("ERR - Uncaught Exception with Auth:");
+			console.error(e);
+		}
+		redirect("/LoginPage");
+	}
+
 	return (
 		
 		<div>
