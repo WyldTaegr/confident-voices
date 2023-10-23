ace242f6e416667ba914bb7be582cff1c2f237b0
Dhananjay Surti <djsurti3003@gmail.com>
2023-10-22 22:39:13 -0400

    Adding exercise option

--- src/app/Application/addquestions/addexercise/page.js
+++ src/app/Application/addquestions/addexercise/page.js
@@ -1,16 +1,54 @@
+'use client'
 // pages/index.js
-import React from 'react';
+import React, { useState , useEffect} from 'react';
 import Link from 'next/link';
-//import Route from "react-router-dom";
-//import Button from 'react-bootstrap/Button'
+import { Amplify } from 'aws-amplify';
+import { API, graphqlOperation } from 'aws-amplify';
+import { useRouter } from 'next/navigation';
+import * as mutations from '@/graphql/mutations';
+import { Button } from '@aws-amplify/ui-react';
+import awsExports from '@/aws-exports';
+Amplify.configure(awsExports);
 
-const AddExercise = () => {
-  //Here, the therapist will choose a student. Then, person will give feedback.
+
+/**
+ * Method to add a question to the set of questions already present in an exercise
+ * @param {*} param0 
+ */
+const Exercise = ({ params }) => {
+  async function exerciseCreation(event, inputValue){
+    event.preventDefault();
+    const newQuestion = await API.graphql(
+      graphqlOperation(mutations.createExercise, { input: {
+        name: inputValue,
+      } })
+    );
+  }
+const router = useRouter();
+const [inputValue, setInputValue] = useState('');
+
+
+  //Function to handle input to the question field
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+
   return (
     <div>
-      <h1> omg add an exercise</h1>    
+     <p>Exercise: {params.slug}</p> 
+     <form onSubmit={(e) => exerciseCreation(e, inputValue)}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter something"
+        />
+        <button type="submit">Add Exercise</button>
+      </form>
     </div>
+    
   );
 };
 
-export default AddExercise;
+export default Exercise;
