62d6aa2b93621ef6f7be70376da614465f479ab5
Tiger Tian <tst25@case.edu>
2023-11-11 17:45:59 -0800

    Remove extraneous code

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,16 +1,7 @@
 import LoginForm from '@/components/LoginForm';
-import { Auth } from 'aws-amplify';
 
 
 export default async function TestPage() {
-
-    let user;
-    try {
-        user = await Auth.currentAuthenticatedUser();
-    } catch (e) {
-        user = e;
-    }
-    const output = user === null ? "NULL" : user.toString();
     return (
         <LoginForm />
     )
