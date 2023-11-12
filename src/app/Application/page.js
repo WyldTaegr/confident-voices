22d38002d80b2d4213713dc2f4893dc35091f659
Tiger Tian <tst25@case.edu>
2023-11-11 18:01:56 -0800

    Remove test API call from application page

--- src/app/Application/page.js
+++ src/app/Application/page.js
@@ -4,11 +4,6 @@ import { API } from 'aws-amplify';
 import * as queries from '@/graphql/queries';
 
 export default async function ApplicationPage() {
-  let output = ""
-  const users = await API.graphql({query: queries.listExercises});
-  output += JSON.stringify(users, null, 4);
-
-  
   return (
     <div className="text-black">
       ApplicationPage
