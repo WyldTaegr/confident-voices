23f8ffc24fab687d330c05bdd0ec5a313628ed0b
Tiger Tian <tst25@case.edu>
2023-12-01 15:08:49 -0800

    Add basic styling to Therapist dashboard

--- src/app/Application/Therapist/page.js
+++ src/app/Application/Therapist/page.js
@@ -2,8 +2,9 @@
 
 import { connectStudentToTherapist, getStudentsByTherapist, listStudents, listUsers } from "@/util/api";
 import { getCurrentUser } from "@/util/auth";
-import { Button } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
+import Link from 'next/link';
 
 export default function TherapistDashboard() {
     const [therapistId, setTherapistId] = useState();
@@ -35,20 +36,31 @@ export default function TherapistDashboard() {
     }
 
     return (
-        <div>
-            <h1>Therapist Dashboard</h1>
-            <h2>Student List</h2>
-            {studentList.map((student) => {
-                return (
-                    <p key={student}>{student}</p>
-                )
-            })}
-            <h2>User List</h2>
-            {userList.map((student) => {
-                return !studentList.includes(student.id) && (
-                    <Button key={student.id} variant="outlined" id={student.id} onClick={() => addStudent(student.id)}>{student.id}</Button>
-                )
-            })}
-        </div>
+        <Box sx={ {display: 'flex', flexDirection: 'column', gap: 2, alignItems: 'center', justifyContent: 'start', height: '100vh', bgcolor: 'background.default', color: 'text.primary' }}>
+            
+            <Typography variant="h4" sx={{ textAlign: 'center', padding: 2 }}>
+                Therapist Dashboard
+            </Typography>
+            <Paper sx={{ display: 'flex', flexDirection: 'column', padding: 2, gap: 2 }}>
+                <Typography variant="h5">Your Students</Typography>
+                <Box sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
+                    {studentList.map((student) => {
+                        return (
+                            <Link key={student} href={`Therapist/s/${student}`} passHref><Button variant="text">{student}</Button></Link>
+                        )
+                    })}
+                </Box>
+            </Paper>
+            <Paper sx={{ display: 'flex', flexDirection: 'column', padding: 2, gap: 2 }}>
+                <Typography variant="h5">Add Student</Typography>
+                <Box sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
+                    {userList.map((student) => {
+                        return !studentList.includes(student.id) && (
+                            <Button key={student.id} variant="outlined" id={student.id} onClick={() => addStudent(student.id)}>{student.id}</Button>
+                        )
+                    })}
+                </Box>
+            </Paper>
+        </Box>
     )
 }
